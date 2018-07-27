ff44760acf18c406eac9b64966ed581883fe22da
VISUAL\mitchel <visualnationnl@gmail.com>
2018-07-27 14:41:17 +0200

    little update

--- out/artifacts/web_war_exploded/resources/js/Charts.js
+++ out/artifacts/web_war_exploded/resources/js/Charts.js
@@ -1,29 +1,70 @@
-var myCanvas = document.getElementById("chart");
-myCanvas.width = 300;
-myCanvas.height = 300;
+let chartSetup = (function () {
+
+    return {
+        canvasSetup: function (element, canvasSize) {
+            let myCanvas = document.getElementById(element);
+            myCanvas.width = canvasSize;
+            myCanvas.height = canvasSize;
+        },
+
+        drawLine: function (ctx, startX, startY, endX, endY) {
+            ctx.beginPath();
+            ctx.moveTo(startX, startY);
+            ctx.lineTo(endX, endY);
+            ctx.stroke();
+        },
+
+        drawArc: function (ctx, centerX, centerY, radius, startAngle, endAngle) {
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+            ctx.stroke();
+        },
+
+        drawPieSlice: function (ctx, centerX, centerY, radius, startAngle, endAngle, color) {
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(centerX, centerY);
+            ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+})();
 
 
-function drawLine(ctx, startX, startY, endX, endY){
-    ctx.beginPath();
-    ctx.moveTo(startX,startY);
-    ctx.lineTo(endX,endY);
-    ctx.stroke();
-}
+let charts = (function (setup) {
 
-function drawArc(ctx, centerX, centerY, radius, startAngle, endAngle){
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-    ctx.stroke();
-}
+    return {
 
-function drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(centerX,centerY);
-    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-    ctx.closePath();
-    ctx.fill();
-}
+        pieChart: function (options) {
+
+            let color = options.color;
+            let customCanvas = options.canvas;
+            let ctx = customCanvas.getContext("2d");
+
+            this.draw = function () {
+                let totalValue = 0;
+                let colorIndex = 0;
+                let startAngle = 0;
+
+                for (categ in options.data) {
+                    let val = options.data[categ];
+                    totalValue += val;
+
+                    let sliceAngle = 2 * Math.PI * val / totalValue;
+                    chartSetup.drawPieSlice(ctx, 150, 150, 100, startAngle, startAngle + sliceAngle, color[colorIndex]);
+
+                    startAngle += sliceAngle;
+                    colorIndex++;
+                }
+            };
+        }
+    }
+})(chartSetup);
+
+
+let myCanvas = chartSetup.canvasSetup('chart', 300);
+
 
 let dataSet = {
     'one': 10,
@@ -33,31 +74,9 @@ let dataSet = {
 };
 
 
-let pieChart = function(options){
 
-    let color = options.color;
-    let customCanvas = options.canvas;
-    let ctx = customCanvas.getContext("2d");
 
-    this.draw = function(){
-        let totalValue = 0;
-        let colorIndex = 0;
-        let startAngle = 0;
-
-        for(categ in options.data){
-            let val = options.data[categ];
-            totalValue += val;
-
-            let sliceAngle = 2 * Math.PI * val / totalValue;
-            drawPieSlice(ctx, 150, 150, 100, startAngle, startAngle + sliceAngle, color[colorIndex]);
-
-            startAngle += sliceAngle;
-            colorIndex++;
-        }
-    };
-};
-
-let myPieChart = new pieChart({
+let myPieChart = charts.pieChart({
     canvas: myCanvas,
     data:dataSet,
     color:["#fde23e","#f16e23", "#57d9ff","#937e88"]
